e8cae198cf58ba9b2fd45cc2c3ab3dbcab231421
ebrahim <ebrahimsameh16@gmail.com>
2022-07-18 03:08:02 +0200

    ..

--- iplookup/urls.py
+++ iplookup/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import IpLookup, HomeView
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', HomeView, name='home'),
     path('iplookup/', IpLookup, name='iplookup'),
+    path('iplookup/', IpLookup, name='whois'),
 
 ]
--- iplookup/views.py
+++ iplookup/views.py
@@ -2,14 +2,45 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 import ipapi
 import requests
+import socket
+from django.template.loader import render_to_string
+from bs4 import BeautifulSoup
+import requests
 
-class HomeView(TemplateView):
-    template_name = 'iplookup/home.html'
-def IpLookup(requests):
-    search = requests.POST.get('search')
+# def visitor_ip_address(request):
+#
+#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+#
+#     if x_forwarded_for:
+#         ip = x_forwarded_for.split(',')[0]
+#     else:
+#         ip = request.META.get('REMOTE_ADDR')
+#     return ip
+
+def visitor_ip_address(request):
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+
+def HomeView(request):
+    mainip = visitor_ip_address(request)
+    context = {"ip": mainip}
+    return render(request,'iplookup/home.html',context)
+
+
+
+def IpLookup(request):
+    search = request.POST.get('search')
     data = ipapi.location(ip=search, output='json')
     context = {"data":data}
-    return render(requests,'iplookup/iplookup.html', context)
+    return render(request,'iplookup/iplookup.html', context)
 
 # def Index(request):
 #      search = request.POST.get('search')
